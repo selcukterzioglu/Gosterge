a15496a7d834ac105b8aa14e71ccdd12f0dab985
strz35 <strz35@hotmail.com.tr>
2018-03-05 01:04:29 +0300

    Update Gosterge_ve_ADC.ino

--- examples/Gosterge_ve_ADC/Gosterge_ve_ADC.ino
+++ examples/Gosterge_ve_ADC/Gosterge_ve_ADC.ino
@@ -2,22 +2,21 @@
 #include <TimerOne.h>
 /*
         A
-      ─────   
+      ───   
     F|  G  |B  
-      ─────   
+      ───   
     D|     |C  
-      ─────   .DP
+      ───   .DP
         C
      Segment bağlantıları (DP display Nokta)
      DP=9, G=8, F=7, E=6, D=5, C=4, B=3, A=2     
      
-     ────    ────    ────
+     ──     ──    ── 
     |    |  |    |  |    |
-     ────    ────    ────  .........
+     ──     ──    ──  ......... Tavsiye edilen maksimum display sayısı 8
     |    |  |    |  |    |
-     ────    ────    ────
-     Anot0   Anot1   Anot2 .........
-
+     ──     ──    ──
+     Anot0   Anot1  Anot2  
  */
 int segmentler[]={9,8,7,6,5,4,3,2};
 int anotlar[]={10,11,12};
