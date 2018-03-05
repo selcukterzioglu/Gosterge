84328ff40147172e129250e411180e9f440328ef
strz35 <strz35@hotmail.com.tr>
2018-03-06 01:39:52 +0300

    Update Gosterge.cpp

--- Gosterge.cpp
+++ Gosterge.cpp
@@ -28,6 +28,7 @@ void Gosterge::gostergeGuncelle(int data)
 	}
 	int rakam[8];
 	rakamAyir(data, rakam);
+	sifiriKaldir(rakam, ONDALIK_YOK);
 	gostergeGonder(rakam[display]);
 	digitalWrite(_suruculer[display], HIGH ^ _ortakAnotMu);
 	display++;
@@ -43,6 +44,7 @@ void Gosterge::gostergeGuncelle(int data, int ondalikSayisi)
 	}
 	int rakam[8];
 	rakamAyir(data, rakam);
+	sifiriKaldir(rakam, ondalikSayisi);
 	gostergeGonder(rakam[display]);
 	if ((_displaySayisi - ondalikSayisi - 1) == display)
 		digitalWrite(_segmentler[0], HIGH ^ _ortakAnotMu);
@@ -52,15 +54,38 @@ void Gosterge::gostergeGuncelle(int data, int ondalikSayisi)
 	display++;
 }
 
+void Gosterge::gostergeGuncelle(int data, int ondalikSayisi, int ozelKarakter, boolean pozisyon)
+{
+	static int display = 0;
+	if (display > _displaySayisi - 1)
+		display = 0;
+	for (int i = 0; i < _displaySayisi; i++) {
+		digitalWrite(_suruculer[i], LOW ^_ortakAnotMu);
+	}
+	int rakam[8];
+	rakamAyir(data, rakam);
+	sifiriKaldir(rakam, ondalikSayisi);
+	if (pozisyon == BASA_EKLE)
+		rakam[0] = ozelKarakter;
+	else
+		rakam[_displaySayisi - 1] = ozelKarakter;
+	gostergeGonder(rakam[display]);
+	if ((_displaySayisi - ondalikSayisi - 1) == display)
+		digitalWrite(_segmentler[0], HIGH ^ _ortakAnotMu);
+	else
+		digitalWrite(_segmentler[0], LOW ^ _ortakAnotMu);
+	digitalWrite(_suruculer[display], HIGH ^ _ortakAnotMu);
+	display++;
+}
 
 void Gosterge::gostergeGonder(uint8_t dispData)
 {
-	if (dispData > 9) {
-		for (int i = 0; i < 8; i++) {
-			digitalWrite(_segmentler[i], 0 ^ _ortakAnotMu);
-		}
-		return;
-	}
+	//if (dispData > 9) {
+	//	for (int i = 0; i < 8; i++) {
+	//		digitalWrite(_segmentler[i], 0 ^ _ortakAnotMu);
+	//	}
+	//	return;
+	//}
 	for (int i = 0; i < 8; i++) {
 		digitalWrite(_segmentler[i], GOSTERGE_BILGISI[dispData][i] ^ _ortakAnotMu);
 	}
@@ -81,4 +106,29 @@ void Gosterge::rakamAyir(int data,int * rakamlar)
 		}
 		
 	}
+}
+
+void Gosterge::sifiriKaldir(int * rakamlar, int ondalikSayisi)
+{
+
+	if (rakamlar[0] > 0)
+		return;
+	for (int i = 0; i < _displaySayisi; i++) {
+		if (i == ondalikSayisi)
+			return;
+		if (i == 0) {
+			if (rakamlar[i] == 0)
+				rakamlar[i] = D_NULL;
+		}
+		else
+		{
+			if (rakamlar[i - 1] == 0)
+				if (rakamlar[i] == 0)
+					rakamlar[i] = D_NULL;
+				else
+					return;
+			else
+				return;
+		}
+	}	
 }
